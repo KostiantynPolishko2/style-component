9d6afe0731782de5bd4b36d44bbae6978d14aaa6
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-21 14:44:06 +0300

    1 Init project of styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import 'the-new-css-reset';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Figure and styled-components</h1>
+    <App/>
   </React.StrictMode>
 );
 
