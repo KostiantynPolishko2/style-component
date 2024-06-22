702668a500b68893284c4f34d6ff2befd1f4914e
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-22 19:46:24 +0300

    10 Events handler and its function had been linked with styled-components
    The bounding had been realized with attrs of styled-components.

--- src/components/Figures/General.styled.ts
+++ src/components/Figures/General.styled.ts
@@ -1,3 +1,4 @@
+import { ReactElement, ReactHTML, ReactHTMLElement } from "react";
 import styled, {keyframes} from "styled-components";
 
 export const CenterPosition = styled.div`
@@ -7,10 +8,32 @@ export const CenterPosition = styled.div`
     align-items: center;
 `;
 
+function bgColor() {
+
+    let bgColor: string = 'red'
+    console.log('setBgColor');
+
+    return (_element: React.FormEvent<HTMLElement>) => {
+        if(bgColor === 'red'){
+            _element.currentTarget.style.backgroundColor = 'grey';
+            bgColor = 'grey';
+            console.log('if', bgColor);
+         }
+         else{
+            _element.currentTarget.style.backgroundColor = 'red';
+            bgColor = 'red';
+            console.log('else', bgColor);
+         }
+    }
+ }
+
+ const setBgColor = bgColor();
+
 export const ButtonStart = styled.button.attrs({
     type: 'button',
     name: 'start-anime',
     value: 'anime',
+    onClick: setBgColor,
 })`
     background-color: red;
     border: 1px solid black;
